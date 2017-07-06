a60f37875d5afffd0ec699ba19361b49b93530f3
dataiyung <dataiyung@gmail.com>
2017-07-06 11:17:46 +0800

    implement part2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def require_is_admin
+  if current_user.admin?
+    flash[:alert] = '你不是管理員 壞壞！'
+    redirect_to root_path
+  end
+  end
 end
